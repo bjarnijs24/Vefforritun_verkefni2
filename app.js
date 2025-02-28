e6a5c74be247e8c802875615ccc5f253f6d240b8
Bjarni Jón Sveinbjörnsson <bjarnijs24@ru.is>
2025-02-28 21:45:25 +0000

    Jæja
    !

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//Vika 5 [20:00] vídjóHarpa
-
 //The URL to which we will send the request
 const API_URL = "http://localhost:3000/api/v1/game-state";
 
@@ -9,6 +7,17 @@ document.getElementById("pad-red").disabled = true;
 document.getElementById("pad-yellow").disabled = true;
 document.getElementById("pad-green").disabled = true;
 document.getElementById("pad-blue").disabled = true;
+const long_time = 500;
+const short_time = 250;
+const modal = document.getElementById("failure-modal");
+
+const showModal = () => {
+    modal.style.display = "block";
+};
+
+const hideModal = () => {
+    modal.style.display = "none"
+};
 
 const startUpReset = async () => {
     try {
@@ -26,21 +35,24 @@ const startUpReset = async () => {
     }
 };
 
-
-const modal = document.getElementById("failure-modal");
-
-const showModal = () => {
-    modal.style.display = "block";
-}
-
-const hideModal = () => {
-    modal.style.display = "none"
-}
+const updateInfoAndPlay = (response) => {
+    //   Update hi score:
+    gameHighScore = response.data.gameState.highScore;
+    document.getElementById("high-score").textContent = gameHighScore;
+      
+    //   update level:
+    level = response.data.gameState.level;
+    document.getElementById("level-indicator").textContent = level;
+        
+    // Get and play sequence:
+    new_sequence = response.data.gameState.sequence;
+    playSequence(new_sequence);
+};
 
 const putGameState = async () => {
     //Perform a GET request to the url
     try {
-        lost = false
+        lost = false;
         hideModal();
         document.getElementById("start-btn").disabled = true;
         document.getElementById("replay-btn").disabled = false;
@@ -53,18 +65,7 @@ const putGameState = async () => {
         //When successful, print the received data
         console.log("Success: ", response.data);
 
-        //   Update hi score:
-        gameHighScore = response.data.gameState.highScore       
-        document.getElementById("high-score").textContent = gameHighScore
-      
-        //   update level:
-        level = response.data.gameState.level
-        document.getElementById("level-indicator").textContent = level
-        
-        // Get and play sequence:
-        new_sequence = response.data.gameState.sequence
-        playSequence(new_sequence)
-        console.log(new_sequence)
+        updateInfoAndPlay(response);
 
     } catch (error) {
       //When unsuccessful, print the error.
@@ -73,6 +74,15 @@ const putGameState = async () => {
     // This code is always executed, independent of whether the request succeeds or fails.
 };
 
+const padActivator = (farb) => {
+    setTimeout(() => {
+        document.getElementById(`pad-${farb}`).classList.add('active');
+    }, short_time);
+
+    setTimeout(() => {
+        document.getElementById(`pad-${farb}`).classList.remove('active');
+    }, long_time);
+};
 
 // Play for user:
 const playSequence = (seq) => {
@@ -80,18 +90,23 @@ const playSequence = (seq) => {
         setTimeout(() => {
             if (element === "red") {
                 synth.triggerAttackRelease("c4", "4n");
+                padActivator(element);
+
             } if (element === "yellow") {
                 synth.triggerAttackRelease("d4", "4n");
+                padActivator(element);
+
             } if (element === "green") {
                 synth.triggerAttackRelease("e4", "4n");
+                padActivator(element);
+
             } if (element === "blue") {
                 synth.triggerAttackRelease("f4", "4n");
+                padActivator(element);
             }
         }, index * 1000);
     });
 };
-
-
 
 // replay:
 const replaySequence = () => {
@@ -116,25 +131,11 @@ const testSequence = async (user_seq) => {
     user_sequence = [];
 
     if (lost !== true) {
-    //   Update hi score:
-    gameHighScore = response.data.gameState.highScore       
-    document.getElementById("high-score").textContent = gameHighScore
-      
-    //   update level:
-    level = response.data.gameState.level
-    document.getElementById("level-indicator").textContent = level
-
-    // Get and play sequence:
-    new_sequence = response.data.gameState.sequence;
-    console.log('New sequence:', response.data.gameState.sequence);
-    playSequence(new_sequence);
-
+        updateInfoAndPlay(response);
     }
 };
 
-
-
-//We initialise the synthesiser with sine wave as default
+// Útfæra synþann
 const synth = new Tone.Synth({ 
     oscillator: { type: "sine" }
     }).toDestination();
@@ -149,49 +150,44 @@ soundSelector.addEventListener("change", (event) => {
     });
 });
 
-// // Hér verður fallið fyrir hljóðið frá database:
-// const playTune = () => {
-//     //initialise a timer to decide when to play individual notes
-//     const now = Tone.now();
-//     //Play a C4 as an 8th note
-//     synth.triggerAttackRelease("c4", "8n", now);
-//     //Play a F#4 as a 4th note, after half a second
-//     synth.triggerAttackRelease("f#4", "4n", now + 0.5);
-//     //Play a D#4 as a half note after 1 second
-//     synth.triggerAttackRelease("d#4", "2n", now + 1);
-// };
+const testSender = (user_sequence) => {
+    if (user_sequence.length === new_sequence.length) {
+        setTimeout(() => {
+            testSequence(user_sequence);
+        },  long_time + short_time);
+    }
+};
 
 const playRed = () => {
     synth.triggerAttackRelease("c4", "4n");
     user_sequence.push("red");
-    console.log(user_sequence)
-    if (user_sequence.length === new_sequence.length) {
-        testSequence(user_sequence)
-    }
+    padActivator("red");
+
+    testSender(user_sequence);
 };
+
 const playYellow = () => {
     synth.triggerAttackRelease("d4", "4n");
     user_sequence.push("yellow");
-    console.log(user_sequence)
-    if (user_sequence.length === new_sequence.length) {
-        testSequence(user_sequence)
-    }
+    padActivator("yellow"); 
+
+    testSender(user_sequence);
 };
+
 const playGreen = () => {
     synth.triggerAttackRelease("e4", "4n");
     user_sequence.push("green");
-    console.log(user_sequence)
-    if (user_sequence.length === new_sequence.length) {
-        testSequence(user_sequence)
-    }
+    padActivator("green");
+
+    testSender(user_sequence);
 };
+
 const playBlue = () => {
     synth.triggerAttackRelease("f4", "4n");
     user_sequence.push("blue")
-    console.log(user_sequence)
-    if (user_sequence.length === new_sequence.length) {
-        testSequence(user_sequence)
-    }
+    padActivator("blue");
+
+    testSender(user_sequence);
 };
 
 // KeyboardListener:
